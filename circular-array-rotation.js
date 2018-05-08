04166e36df6e9c1f8570fda52cb695636c1512f4
Daniel Szabo <dgszabo@gmail.com>
2018-05-07 19:29:46 -0700

    Circular array rotation array manipulation exercise - solved

---
+++ circular-array-rotation.js
@@ -0,0 +1,22 @@
+// CIRCULAR ARRAY ROTATION ARRAY MANIPULATION EXERCISE
+// HackerRank
+function circularArrayRotation(a, k, m) {
+    let outArr = []
+    
+    let newArr = []
+    a.forEach((el, idx) => {
+        if(idx + (k % a.length) >= a.length) {
+            newArr[idx + (k % a.length) - a.length] = a[idx];
+        } else {
+            newArr[idx + (k % a.length)] = a[idx];            
+        }
+    });
+    
+    m.forEach(el => {
+       outArr.push(newArr[el]); 
+    });
+    
+    return outArr;
+}
+
+circularArrayRotation([1, 2, 3], 2, [0, 1, 2])
