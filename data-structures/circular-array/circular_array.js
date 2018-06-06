b440e0fd28ed7bffef2fe7e877e910239aaa5664
Daniel Szabo <dgszabo@gmail.com>
2018-06-06 09:50:05 -0700

    Circular array problem - solved in JS in O(n) rotational time and space

--- data-structures/circular-array/circular_array.js
+++ data-structures/circular-array/circular_array.js
@@ -1,11 +1,26 @@
+// CIRCULAR ARRAY PROBLEM
+// Rithm School
+// in O(n) rotational time and O(n) rotational space complexity
 class CircularArray {
-	constructor() { }
+	constructor() { 
+        this.arr = [];
+	}
 
-	addItem(item) { }
-	getByIndex(index) { }
-	rotate(increment) { }
-	printArray() { }
+	addItem(item) { 
+        this.arr.push(item);
+	}
+	getByIndex(index) { 
+        if(index >= 0 && index < this.arr.length) return this.arr[index];
+        else return `There is no element on index ${index}!`;
+	}
+	rotate(increment) { 
+        let tempArr = [...this.arr]
+        for(let i = 0; i < this.arr.length; i++) {
+            tempArr[i] = this.arr[(i + increment) % this.arr.length];
+        }
+        this.arr = [...tempArr];
+	}
+	printArray() { 
+        console.log(this.arr.join('\n'));
+	}
 }
-
-
-
