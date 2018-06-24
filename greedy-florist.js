28901da00779b328f6ddf18d22983696d27e7021
Daniel Szabo <dgszabo@gmail.com>
2018-06-24 13:57:05 -0700

    Greedy florist greedy algo problem - solution fixed in JS

--- greedy-florist.js
+++ greedy-florist.js
@@ -7,21 +7,14 @@ function getMinimumCost(k, c) {
         return c.reduce((acc, a) => acc + a);
     } else {
         let acc = 0;
-        for(let i = 0; i < k; i++) {
-            acc += c.pop()
-        }
-
-        let multi = 1;
-        let skip = Math.ceil(c.length / k);
-        let counter = 0;
-        while(c.length > counter) {
-            for(let i = multi - 1; i < c.length; i += skip) {
-                if(c[i]) {
-                    acc += (multi + 1) * c[i];
-                    counter++;
+        let multi = 0;
+        while(c.length > 0) {
+            for(let i = 0; i < k; i++) {
+                if(c.length > 0) {
+                    acc += (multi + 1) * c.pop();
                 }
             }
-            multi++;
+            multi++;    
         }
         
         return acc;
