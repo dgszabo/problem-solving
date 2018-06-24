9c6f2c0e948367142cd1e37b2a44a682560a7d10
Daniel Szabo <dgszabo@gmail.com>
2018-06-24 16:12:03 -0700

    Candies dynamic programming problem - solved in JS

--- candies.js
+++ candies.js
@@ -1,5 +1,27 @@
 // CANDIES DYNAMIC PROGRAMMING PROBLEM
 // HackerRank https://www.hackerrank.com/challenges/candies/problem
+// O(n) time complexity solution
+function candies(n, arr) {
+    let candiesArr = Array.apply(null, Array(arr.length)).map((el) => 1);
+
+    let i = 0;
+
+    while(i < arr.length) {
+        if(arr[i - 1] && arr[i] > arr[i - 1] && candiesArr[i] <= candiesArr[i - 1]) {
+            candiesArr[i] = Math.max(candiesArr[i - 1] + 1, candiesArr[i]);
+        } else if(arr[i + 1] && arr[i] > arr[i + 1] && candiesArr[i] <= candiesArr[i + 1]) {
+            candiesArr[i] = Math.max(candiesArr[i + 1] + 1, candiesArr[i]);
+        } else if(arr[i - 1] && arr[i] < arr[i - 1] && candiesArr[i] >= candiesArr[i - 1]) {
+            i--;
+        } else {
+            i++;
+        }
+    }
+
+    return candiesArr.reduce((sum, a) => sum + a, 0);
+}
+
+// O(n2) time complexity solution
 function candies(n, arr) {
     let min = Math.min(...arr);
     let max = Math.max(...arr);
