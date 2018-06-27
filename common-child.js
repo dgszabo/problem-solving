cdab3759074072251af81f078dadc469ed912ea2
Daniel Szabo <dgszabo@gmail.com>
2018-06-27 09:32:19 -0700

    Common child algo problem - partially solved in JS

---
+++ common-child.js
@@ -0,0 +1,19 @@
+// COMMON CHILD PROBLEM
+// HackerRank https://www.hackerrank.com/challenges/common-child/problem
+function commonChild(s1, s2) {
+    let common = [];
+    let s2Counter = 0;
+    
+    for(let i = 0; i < s1.length; i++) {
+        let first = true; 
+        for(let j = s2Counter; j < s2.length; j++) {
+            if(s1[i] === s2[j] && first) {
+                common.push(s1[i]);
+                s2Counter = j + 1;
+                first = false;
+            }
+        }
+    }
+
+    return common.length;
+}
