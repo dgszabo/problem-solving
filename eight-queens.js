b3fdc8ea7ad761b9b450acb9f861b0a07211b493
Daniel Szabo <dgszabo@gmail.com>
2018-06-30 20:38:00 -0700

    Eight queens matrix problem - solved in JS

---
+++ eight-queens.js
@@ -0,0 +1,52 @@
+// EIGHT QUEENS MATRIX PROBLEM
+function validatePosition(arr, y) {
+    // create slanted variables
+    let rightUp = y;
+    let leftUp = y;
+
+    // iterate through the already placed queens array and check if the new pos is invalid
+    for(let i = arr.length - 1; i >= 0; i--) {
+        rightUp++;
+        leftUp--;
+        if(arr[i] === y || (rightUp < 8 && arr[i] === rightUp) || (leftUp >= 0 && arr[i] === leftUp)) {
+            return false;
+        }
+    }
+    // otherwise return true
+    return true;
+}
+
+function eightQueens(positions = [], y = 0, results = []) {
+    // return results when exceed the end of the first line
+    if(positions.length === 0 && y === 8) {
+        return results;
+    }
+    // if(results.length === 2) {
+    //     return results;
+    // }
+
+    // if the positions array is 8 long, add it to the results array
+    if(positions.length === 8) {
+        results.push([...positions]);
+        
+        // and continue search with new last position
+        let newY = positions.pop() + 1;
+        return eightQueens([...positions], newY, results);
+    }
+
+    // if the position is out of range roll back to last line
+    if(y >= 8) {
+        let newY = positions.pop() + 1;
+        return eightQueens([...positions], newY, results);
+    } else {
+        // if the position in the line validates continue to next line, else try next position
+        if(validatePosition(positions, y)) {
+            positions.push(y);
+            return eightQueens([...positions], 0, results);
+        } else {
+            return eightQueens([...positions], y + 1, results);
+        }    
+    }
+}
+
+console.log(eightQueens());
