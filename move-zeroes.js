0acdb076873f7e8bf754499f12914cc50220754b
Daniel Szabo <dgszabo@gmail.com>
2018-07-07 23:51:39 -0700

    Move zeroes array problem - in-place swapping solution in JS

--- move-zeroes.js
+++ move-zeroes.js
@@ -16,3 +16,26 @@ function moveZeroes(arr) {
 
   return arr;
 }
+
+// in-place swapping solution
+function moveZeroes(arr) {
+  let zeroes = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+      if(arr[i] === 0) zeroes++;
+  }
+
+  for(let i = 0; i < arr.length - zeroes; i++) {
+      if(arr[i] === 0) {
+          let swapIdx = i;
+          while(arr[swapIdx] === 0 && swapIdx < arr.length - 1) {
+              swapIdx++;
+          }
+          if(arr[swapIdx] !== 0) {
+              [arr[i], arr[swapIdx]] = [arr[swapIdx], arr[i]];
+          }
+      }
+  }
+
+  return arr;
+}
