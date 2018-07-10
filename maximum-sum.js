42faff7fbd1aedb6646a0cf42aa6897a0c69d23a
Daniel Szabo <dgszabo@gmail.com>
2018-07-09 23:00:02 -0700

    Maximum sum array manipulation problem - solved in JS

---
+++ maximum-sum.js
@@ -0,0 +1,20 @@
+// MAXIMUM SUM ARRAY MANIPULATION PROBLEM
+// HackerRank https://www.hackerrank.com/challenges/maximum-subarray-sum/problem
+function maximumSum(a, m) {
+  let max = 0;
+
+  // try all possible sum combinations
+  for(let i = 0; i < a.length; i++) {
+      let sum = 0;
+      for(let j = i; j < a.length; j++) {
+          sum += a[j];
+
+          // check every time if this sum % is larger than the max
+          if(sum % m > max) {
+              max = sum % m;
+          }
+      }
+  }
+
+  return max;
+}
