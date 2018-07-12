71a1b30df09eeda23b37dd0f84852838405d7e93
Daniel Szabo <dgszabo@gmail.com>
2018-07-12 07:43:29 -0700

    Max min greedy problem - solved in JS in O(n lolg n)

--- max-min.js
+++ max-min.js
@@ -1,5 +1,6 @@
 // MAX MIN GREEDY ALGO PROBLEM
 // HackerRank https://www.hackerrank.com/challenges/angry-children/problem
+// O(n2) runtime
 function maxMin(k, arr) {
   // sort arr
   arr.sort((a,b) => a - b);
@@ -17,6 +18,25 @@ function maxMin(k, arr) {
       win.push(arr[i]);
 
       let tempDiff = Math.max(...win) - Math.min(...win);
+      if(tempDiff < minDiff) {
+          minDiff = tempDiff;
+      }
+  }
+
+  return minDiff;
+}
+
+// O(n log n runtime)
+function maxMin(k, arr) {
+  // sort arr
+  arr.sort((a,b) => a - b);
+
+  // create minDiff var
+  let minDiff = Infinity;
+
+  // slide win and check if win is smaller than mindiff at each step
+  for(let i = 0; i <= arr.length - k; i++) {
+      let tempDiff = arr[i + k - 1] - arr[i];
       if(tempDiff < minDiff) {
           minDiff = tempDiff;
       }
