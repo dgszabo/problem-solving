ed2d1ec3461815d301c894da15ad200e14877fdd
Daniel Szabo <dgszabo@gmail.com>
2018-07-12 09:41:57 -0700

    Eight queens matrix problem - problem description added

--- eight-queens.js
+++ eight-queens.js
@@ -1,4 +1,15 @@
 // EIGHT QUEENS MATRIX PROBLEM
+
+/*
+The eight queens puzzle is the problem of placing eight chess queens on an 8×8 chessboard so that no two queens threaten each other. Thus, a solution requires that no two queens share the same row, column, or diagonal.
+
+Return an array/list with all the possible solutions (either in a 2D matrix each or other data structure with the positions on the board).
+
+Be mindful not to exceed the maximum recursion depth in most JS/python envorinments. This is a long one, so don't worry if you need more than an hour.
+
+Good luck!
+*/
+
 function validatePosition(arr, y) {
     // create slanted variables
     let rightUp = y;
@@ -25,7 +36,7 @@ function eightQueens(positions = [], y = 0, results = [], limit = 1) {
     // if the positions array is 8 long, add it to the results array
     if(positions.length === 8) {
         results.push([...positions]);
-        
+
         // and continue search with new last position
         let newY = positions.pop() + 1;
         return eightQueens(positions, newY, results, limit);
@@ -42,14 +53,14 @@ function eightQueens(positions = [], y = 0, results = [], limit = 1) {
             return eightQueens(positions, 0, results, limit);
         } else {
             return eightQueens(positions, y + 1, results, limit);
-        }    
+        }
     }
 }
 
 // result compiler function to circumvent exceeding max stack size issue
 function compileResults() {
     let allConfigurations = [];
-    
+
     for(let i = 0; i < 8; i++) {
         allConfigurations.push(...eightQueens([], i, [], i + 1));
     }
