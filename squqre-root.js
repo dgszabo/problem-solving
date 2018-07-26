820baee0e06ed4a3a59ac152e8fe3639f43707c0
Daniel Szabo <dgszabo@gmail.com>
2018-07-26 16:00:46 -0700

    Square root binary search problem - documentation added

--- squqre-root.js
+++ squqre-root.js
@@ -1,4 +1,13 @@
 // SQUARE ROOT BINARY SEARCH PROBLEM
+
+/*
+Everybody can calculate the square root of a number using Math.sqrt(). Your task is to implement a square root function that takes in a number (integer or float) and returns it's square root with certain pre-determined precision.
+
+Hint: Think of a binary search-like algorithm that continues until it finds a number that approximates the squre root of the number with the predetermined precision.
+
+Good luck!
+*/
+
 function getSqrt(num, prec = 1, low = 0, high = num) {
   // negative number edge case
   if(num < 0) return -1;
