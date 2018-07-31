94fd073f25f8eb6fa1459a16afda46e29984dbd4
Daniel Szabo <dgszabo@gmail.com>
2018-07-30 21:37:25 -0700

    Add padding (justify) problem - documentation added

--- add-padding.js
+++ add-padding.js
@@ -1,3 +1,15 @@
+// ADD PADDING (JUSTIFY) PROBLEM
+/*
+The input is a string with space-separated words in it and an integer. If the integer is larger than the length of the input string, your task is to add extra spaces between the words until the string reaches the desired length (the input integer). Make sure to distribute the spces as evenly as possible; adding all the extra spaces to one position if others are available is not a valid solution.
+
+Examples:
+"hello world", 7 -> "hello world"
+"hello world again", 19 -> "hello  world  again"
+"hello world again", 20 -> "hello   world  again"
+
+Good luck!
+*/
+
 function addPadding(n, str) {
   // invalid input edge cases -> throw error
       // if n is smaller than str.length
