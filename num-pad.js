0f744d414894bf3182bc4d19f33e132669bac80d
Daniel Szabo <dgszabo@gmail.com>
2018-08-01 09:19:23 -0700

    Numpad recursive problem - documentation added

--- num-pad.js
+++ num-pad.js
@@ -1,4 +1,30 @@
 // NUMPAD RECURSION PROBLEM
+
+/*
+Your task is to devise a function that takes a number in string format and returns an array with all the possible combinations of letters as separate strings that can be created pushing the input numbers on a phone keypad. Your function should take (at least) two arguments, a string of N length containing only numbers between 2 and 9 (inclusive) and an object containing an association table that associates the number on a phone keypad with letters of the alphabet.
+
+Examples:
+numPad('2', assocTable) // ['a', 'b', 'c']
+numPad('96', assocTable) // ["xm", "xn", "xo", "ym", "yn", "yo", "zm", "zn", "zo"]
+numPad('234', assocTable) // ["adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg", "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi", "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi"]
+
+// the association table to use
+var assocTable = {
+  '2': ['a', 'b', 'c'],
+  '3': ['d', 'e', 'f'],
+  '4': ['g', 'h', 'i'],
+  '5': ['j', 'k', 'l'],
+  '6': ['m', 'n', 'o'],
+  '7': ['p', 'q', 'r', 's'],
+  '8': ['t', 'u', 'v', 'w'],
+  '9': ['x', 'y', 'z']
+}
+
+Hint: you can write the function in several ways, but the most straight forward is writing it recursively. Don't worry about the recursive iteration limit, the input numbers are not going to be long.
+
+Good luck!
+*/
+
 function numPad(numStr, assocTable) {
   let results = [];
 
