10704fba7586a2d290e8358f8cc2c616a9e0e4b1
Daniel Szabo <dgszabo@gmail.com>
2018-08-06 09:49:27 -0700

    Check sudoku  2D matrix problem - documentation amended

--- check-sudoku.js
+++ check-sudoku.js
@@ -10,16 +10,16 @@ A sudoku board should comply with three requirements:
 
 Example:
 var inputMatrix = [[5,3,4,6,7,8,9,1,2],
-//                  [6,7,2,1,9,5,3,4,8],
-//                  [1,9,8,3,4,2,5,6,7],
-//                  [8,5,9,7,6,1,4,2,3],
-//                  [4,2,6,8,5,3,7,9,1],
-//                  [7,1,3,9,2,4,8,5,6],
-//                  [9,6,1,5,3,7,2,8,4],
-//                  [2,8,7,4,1,9,6,3,5],
-//                  [3,4,5,2,8,6,1,7,9]];
+                  [6,7,2,1,9,5,3,4,8],
+                  [1,9,8,3,4,2,5,6,7],
+                  [8,5,9,7,6,1,4,2,3],
+                  [4,2,6,8,5,3,7,9,1],
+                  [7,1,3,9,2,4,8,5,6],
+                  [9,6,1,5,3,7,2,8,4],
+                  [2,8,7,4,1,9,6,3,5],
+                  [3,4,5,2,8,6,1,7,9]];
 
-// checkSudoku(inputMatrix) // true
+checkSudoku(inputMatrix) // true
 
 Hint: this may end up being a fairly long function with all the different requirements the sudoku board needs to comply with. Try to break out some of the parts into modules to lighten the logic the main function has to take care of.
 
