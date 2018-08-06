de55fa5563ea1f4ca50a2b99e34b36770d1f9b9c
Daniel Szabo <dgszabo@gmail.com>
2018-08-06 10:12:52 -0700

    Circular array rotation problem - documentation added

--- circular-array-rotation.js
+++ circular-array-rotation.js
@@ -1,21 +1,21 @@
 // CIRCULAR ARRAY ROTATION ARRAY MANIPULATION EXERCISE
-// HackerRank
+// HackerRank https://www.hackerrank.com/challenges/circular-array-rotation/problem
 function circularArrayRotation(a, k, m) {
     let outArr = []
-    
+
     let newArr = []
     a.forEach((el, idx) => {
         if(idx + (k % a.length) >= a.length) {
             newArr[idx + (k % a.length) - a.length] = a[idx];
         } else {
-            newArr[idx + (k % a.length)] = a[idx];            
+            newArr[idx + (k % a.length)] = a[idx];
         }
     });
-    
+
     m.forEach(el => {
-       outArr.push(newArr[el]); 
+       outArr.push(newArr[el]);
     });
-    
+
     return outArr;
 }
 
