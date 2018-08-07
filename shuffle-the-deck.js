531c8da27f13621e07237327d2279337a2828543
Daniel Szabo <dgszabo@gmail.com>
2018-08-07 14:06:10 -0700

    Shuffle the deck problem - documentation added

--- shuffle-the-deck.js
+++ shuffle-the-deck.js
@@ -1,4 +1,15 @@
 // SHUFFLE THE DECK CARD SHUFFLING PROBLEM
+
+/*
+Your friend is interested in card tricks; for this they will need a full deck of cards that are shuffled. You need to help them do card tricks on a computer. Your task is to write a function or class that creates a deck of cards and then shuffles the deck. Your function/class should return an array with 52 cards in a random order.
+
+Constraints:
+1) don't use any external libraries.
+2) try to avoid wasteful steps when shuffling the deck and make your shuffling logic as efficient as possible.
+
+Good luck!
+*/
+
 function shuffleTheDeck() {
   // create colors and values
   const colors = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
